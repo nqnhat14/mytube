e847bf5999375276d4fb382b2fc4cda36eab6744
nqnhat14 <nqnhat14@live.com>
2018-10-17 16:41:25 +0700

    Order with authentication

--- server/repository/order.js
+++ server/repository/order.js
@@ -3,26 +3,37 @@ const Order = mongoose.model('Order');
 const Promise = require("bluebird");
 Promise.promisifyAll(mongoose);
 module.exports = {
-    getOrders:()=>{
-        return new Promise((resolve,reject)=>{
-            Order.findAsync()
-                .then(orders=>{
-                    resolve(orders);
-                }).catch(err=>{
-                console.log(err);
-                reject(err);
-            })
+    getOrders: (userId) => {
+        return new Promise((resolve, reject) => {
+            if (userId) {
+                Order.findAsync({})
+                    .then(orders => {
+                        resolve(orders);
+                    }).catch(err => {
+                        console.log(err);
+                        reject(err);
+                    })
+            }
+            else {
+                Order.findAsync()
+                    .then(orders => {
+                        resolve(orders);
+                    }).catch(err => {
+                        console.log(err);
+                        reject(err);
+                    })
+            }
         })
     },
-    saveOrder:(orderData)=>{
+    saveOrder: (orderData) => {
         const order = new Order(orderData);
-        return new Promise((resolve,reject)=>{
+        return new Promise((resolve, reject) => {
             order.saveAsync()
-                .then(order=>{
+                .then(order => {
                     resolve(order);
-                }).catch(err=>{
-                reject(err);
-            })
+                }).catch(err => {
+                    reject(err);
+                })
         })
     }
 };
